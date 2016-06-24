59eda1a4049d1ae0b618d58b54f51d6de4110763
bad-clown <fgmfjf@163.com>
2016-06-24 17:38:23 +0800

    init

---
+++ modules/published.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * 
+ * @authors Your Name (you@example.org)
+ * @date    2016-06-24 17:33:53
+ * @version $Id$
+ */
+
+class ClassName extends AnotherClass {
+    
+    function __construct(){
+        
+    }
+}
